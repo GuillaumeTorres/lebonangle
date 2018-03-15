1dd2fd3caef9cad1a09d934185803be9ada7b3da
tlsosadr15 <tl.sosa.dr@gmail.com>
2018-03-15 12:43:12 +0100

    Mise en place des apis pour les furnitures - @Sosa - 15/03/18

--- src/AppBundle/Controller/FurnitureController.php
+++ src/AppBundle/Controller/FurnitureController.php
@@ -10,7 +10,6 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Routing\Annotation\Route;
 use AppBundle\Entity\Furniture;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
